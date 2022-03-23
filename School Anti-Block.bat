d075b69a53dc83cf5f8195a6b9f0dcc623ab5a73
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2022-03-23 09:15:22 -0500

    Update School Anti-Block.bat

--- School Anti-Block.bat
+++ School Anti-Block.bat
@@ -1,4 +1,10 @@
 @echo off
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') 
+
+do (
+  set "DEL=%%a"
+)
 Color 05
 Title School Anti-Block
 Echo.
@@ -19,3 +25,11 @@ taskkill /F /im LSSASvc.exe
 taskkill /F /im smart-agent-js-win.exe
 taskkill /F /im makeca.exe
 goto x
+:colorEcho
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1\
+Echo.
+Echo.
+Echo An Error Has Ocurred Please Reopen the Batch file.
