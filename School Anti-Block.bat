b624f0c11b7c93c2b9edf34b1525d4b96aecd74b
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2022-03-24 15:34:39 -0500

    Update School Anti-Block.bat

--- School Anti-Block.bat
+++ School Anti-Block.bat
@@ -1,13 +1,15 @@
 @echo off
-Color 05
+setlocal disableDelayedExpansion
+set q=^"
+Color 07
 Title School Anti-Block
 Echo.
-Echo ================================
-Echo    Windows School Anti-Block
-Echo          Created by:
-powershell write-host -fore Cyan           @yodaluca23
-Echo            v1.2.1
-Echo ================================
+call :c 0D "================================" /n
+call :c 0C "    Windows School Anti-Block" /n
+call :c 09 "          Created by:" /n
+call :c 0B "          @yodaluca23" /n
+call :c 0A "            v1.2.1" /n
+call :c 0D "================================" /n
 Echo.
 Echo.
 Echo.
@@ -17,6 +19,7 @@ cls
 :Task
 taskkill /F /im lsproxy.exe > nul 2> nul
 taskkill /F /im LSSASvc.exe > nul 2> nul
+Echo Now attempting to kill processes...
 taskkill /F /im smart-agent-js-win.exe > nul 2> nul
 taskkill /F /im makeca.exe > nul 2> nul
 goto Task
@@ -31,3 +34,57 @@ Echo.
 Echo.
 Echo An Unforscene Error Has Occured Please Reopen The Script.
 Echo.
+:c
+setlocal enableDelayedExpansion
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:colorPrint Color  Str  [/n]
+setlocal
+set "s=%~2"
+call :colorPrintVar %1 s %3
+exit /b
+
+:colorPrintVar  Color  StrVar  [/n]
+if not defined DEL call :initColorPrint
+setlocal enableDelayedExpansion
+pushd .
+':
+cd \
+set "s=!%~2!"
+for %%n in (^"^
+
+^") do (
+  set "s=!s:\=%%~n\%%~n!"
+  set "s=!s:/=%%~n/%%~n!"
+  set "s=!s::=%%~n:%%~n!"
+)
+for /f delims^=^ eol^= %%s in ("!s!") do (
+  if "!" equ "" setlocal disableDelayedExpansion
+  if %%s==\ (
+    findstr /a:%~1 "." "\'" nul
+    <nul set /p "=%DEL%%DEL%%DEL%"
+  ) else if %%s==/ (
+    findstr /a:%~1 "." "/.\'" nul
+    <nul set /p "=%DEL%%DEL%%DEL%%DEL%%DEL%"
+  ) else (
+    >colorPrint.txt (echo %%s\..\')
+    findstr /a:%~1 /f:colorPrint.txt "."
+    <nul set /p "=%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%"
+  )
+)
+if /i "%~3"=="/n" echo(
+popd
+exit /b
+
+
+:initColorPrint
+for /f %%A in ('"prompt $H&for %%B in (1) do rem"') do set "DEL=%%A %%A"
+<nul >"%temp%\'" set /p "=."
+subst ': "%temp%" >nul
+exit /b
+
+
+:cleanupColorPrint
+2>nul del "%temp%\'"
+2>nul del "%temp%\colorPrint.txt"
+>nul subst ': /d
